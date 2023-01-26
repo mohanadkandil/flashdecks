9f3e5a718034f2ea5203efd9f490f58c73ef2b12
mohanadkandil <mohanadmkandil@gmail.com>
2023-01-26 12:45:15 -0800

    chore: add dotenv to secure the app

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,29 @@
+import { config } from "dotenv";
+config();
+
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
 
 import Deck from "./models/Deck";
-import dotenv from "dotenv";
-dotenv.config({ path: __dirname });
+
 const app = express();
-console.log(process.env.MONGO_URI);
+app.use(express.json());
+
 const PORT = 5000;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("welcome to the new app");
 });
 
-mongoose.connect(process.env.MONGO_URI).then(() => {
+app.post("/decks", async (req: Request, res: Response) => {
+  const newDeck = new Deck({
+    title: req.body.title,
+  });
+  const createdDeck = await newDeck.save();
+  res.json(createdDeck);
+});
+
+mongoose.connect(process.env.MONGO_URL!).then(() => {
   console.log(`listening on port ${PORT}`);
   app.listen(PORT);
 });
