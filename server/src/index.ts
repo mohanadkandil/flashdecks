cda01e98ffa2de7e7cfff3b3acab7924844ca6cd
mohanadkandil <mohanadmkandil@gmail.com>
2023-01-27 04:30:56 -0800

    chore: add get single deck in main server

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,8 +6,11 @@ import mongoose from "mongoose";
 import cors from "cors";
 import {
   getDecksController,
+  getDeckController,
   createDeckController,
   deleteDeckController,
+  createCardForDeckController,
+  deleteCardForDeckController,
 } from "./controllers";
 
 const app = express();
@@ -18,9 +21,15 @@ const PORT = 5000;
 
 app.get("/decks", getDecksController);
 
+app.get("/decks/:deckId", getDeckController);
+
 app.post("/decks", createDeckController);
 
+app.post("/decks/:deckId/cards", createCardForDeckController);
+
 app.delete("/decks/:deckId", deleteDeckController);
+
+app.delete("/decks/:deckId/cards/:index", deleteCardForDeckController);
 
 mongoose.connect(process.env.MONGO_URL!).then(() => {
   console.log(`listening on port ${PORT}`);
